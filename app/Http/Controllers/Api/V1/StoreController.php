73a48f644298a9d60beb107efa18cda95db90ab1
abd <info@ezoo.co>
2017-05-09 15:30:23 +0300

    api v1 reterve data

---
+++ app/Http/Controllers/Api/V1/StoreController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Store;
+
+class StoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $store  = Store::orderBy('id','desc')->paginate(20)->toArray();
+
+        if(count($store))
+            $is_success = true;
+
+        return response()->json(['success'=>$is_success ,'message' => [],'data'=>$store], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $store = Store::find($id);
+
+        if(count($store))
+            $this->is_success= true;
+
+        return response()->json(['success'=>$this->is_success ,'message' =>[],'data'=>$store]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('v1/getallcompany', 'Api\V1\CompanyController@index');
+Route::get('v1/getcompany/{id}', 'Api\V1\CompanyController@show');
+Route::get('v1/getmaincategorise', 'Api\V1\CategoriseController@index');
+Route::get('v1/getadvabsedcategorise/{id}', 'Api\V1\CategoriseController@show');
+Route::get('v1/getallstore', 'Api\V1\StoreController@index');
+Route::get('v1/getstore/{id}', 'Api\V1\StoreController@show');
