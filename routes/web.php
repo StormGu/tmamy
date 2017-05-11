fb202d1d175fb961beaf05bde1bca9f21cfd0836
abd <info@ezoo.co>
2017-05-11 14:24:30 +0300

    add static page route -_-

--- routes/web.php
+++ routes/web.php
@@ -260,3 +260,6 @@ Route::get('lang/{lang}', function ($lang) {
 Route::get('images/profile/{id}/avatar/{image}', [
     'uses' => 'ProfilesController@userProfileAvatar'
 ]);
+/** CATCH-ALL ROUTE for Backpack/PageManager - needs to be at the end of your routes.php file  **/
+Route::get('{page}/{subs?}', ['uses' => 'PageController@index'])
+    ->where(['page' => '^((?!admin).)*$', 'subs' => '.*']);
