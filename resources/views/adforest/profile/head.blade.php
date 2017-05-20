1132ceca41d8727d648de9330c9f0fd44a7c94c5
Buthaina94 <buthainasharaf@gmail.com>
2017-05-20 17:16:32 +0300

    fix 78

--- resources/views/adforest/profile/head.blade.php
+++ resources/views/adforest/profile/head.blade.php
@@ -20,7 +20,7 @@
                     </div>
                     <div class="modal-body">
                         <input type="hidden" name="from_name" value="{{ \Auth::user()->name }}">
-                        <input type="hidden" name="to_user_id" value="{{ $user->id }}">
+                        <input type="hidden" name="to_user_id" value="{{ $object->id }}">
                         <input type="hidden" name="from_user_id" value="{{ Auth::id() }}">
                         <textarea name="messages" class="form-control" rows="5" id="comment"
                                   placeholder="Enter Your Message"></textarea>
@@ -42,64 +42,77 @@
         <div class="image" data-user="avatar" data-type="B">
             <img id="imgAvatar"
                  src="@if (\Auth::user()->profile->avatar_status == 1){{ url(\Auth::user()->profile->avatar) }} @else {{ Gravatar::get(\Auth::user()->email) }} @endif">
-        
+        </div>
     </div>
 
     <ul class="st-social">
-        <li><em>{{ \App\Models\Advertisement::where('user_id', $user->id)->count() }}</em><span>ADS</span></li>
+        <li><em>{{ \App\Models\Advertisement::where('user_id', $object->id)->count() }}</em><span>ADS</span></li>
+        <li><em><a href="{{ url('profile/getfollower/'.  $object->id) }}">{{ \App\Models\UserFollower::where('user_id', $object->id)->count() }}</a></em><span><a href="{{ url('profile/getfollower/'.  $object->id) }}">follower</a></span></li>
         <li>
-            <em><a href="{{ url('profile/getfollower/'.  $user->id) }}">{{ \App\Models\UserFollower::where('user_id', $user->id)->count() }}</a></em><span><a
-                        href="{{ url('profile/getfollower/'.  $user->id) }}">follower</a></span></li>
-        <li>
-            <em class="MyCounter">{{ \App\Models\StoreLike::where('user_id', $user->id)->count() }}</em>
+            <em class="MyCounter">{{ \App\Models\StoreLike::where('user_id', $object->id)->count() }}</em>
             <span>
                                         Likes</span>
         </li>
     </ul>
 
+    <ul class="st-social rig100">
+        <form action="{{ url('profile/follower')}}" method="post">
+            {{ csrf_field() }}
+            <input type="hidden" name="user_id" value="{{ $object->id }}">
+            <input type="hidden" name="user_followers_id" value="{{ Auth::id() }}">
+            <input type="hidden" name="user_followers_name" value="{{ \Auth::user()->name }}">
+            @if(\App\Models\UserFollower::where('user_followers_id', Auth::id())->count() == 0)
+                <button type="submit" class="btn btn-default1 rig"> Follow</button>
+            @elseif(\App\Models\UserFollower::where('user_followers_id', Auth::id())->count() > 0)
+        </form>
+                <form action="{{ url('profile/unfollower') }}" method="post">
+                    {{ csrf_field() }}
+                    <input type="hidden" name="user_id" value="{{ $object->id }}">
+                    <input type="hidden" name="user_followers_id" value="{{ Auth::id() }}">
+                    <button type="submit" class="btn btn-default1 rig"> unFollow</button>
+                </form>
+            @endif
+
+
+            <button type="button" class="btn btn-default1 rig1" data-toggle="modal" data-target="#myModal">
+                <a>Messages</a></button>
+    </ul>
     <ul class="socail-share">
-        @if($profile->facebook)
-            <li>
-                <a href="{{ $profile->facebook }}" target="_blank">
-                    <i class="fa fa-facebook" aria-hidden="true"></i>
-                </a>
-            </li>
-        @endif
-        @if($profile->twitter)
-            <li>
-                <a href="{{ $profile->twitter }}" target="_blank">
-                    <i class="fa fa-twitter" aria-hidden="true"></i>
-                </a>
-            </li>
-        @endif
-        @if($profile->google)
-            <li>
-                <a href="{{ $profile->google }}" target="_blank">
-                    <i class="fa fa-google" aria-hidden="true"></i>
-                </a>
-            </li>
-        @endif
-        @if($profile->linkedin)
-            <li>
-                <a href="{{ $profile->linkedin }}" target="_blank">
-                    <i class="fa fa-linkedin" aria-hidden="true"></i>
-                </a>
-            </li>
-        @endif
-        @if($profile->pinterest)
-            <li>
-                <a href="{{ $profile->pinterest }}" target="_blank">
-                    <i class="fa fa-pinterest-p" aria-hidden="true"></i>
-                </a>
-            </li>
-        @endif
+        <li>
+            <a href="{{ $object->facebook }}" target="_blank">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+        </li>
+        <li>
+            <a href="{{ $object->twitter }}" target="_blank">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+        </li>
+        <li>
+            <a href="{{ $object->google }}" target="_blank">
+                <i class="fa fa-google" aria-hidden="true"></i>
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ $object->linkedin }}" target="_blank">
+                <i class="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+        </li>
+        <li>
+            <a href="{{ $object->pinterest }}" target="_blank">
+                <i class="fa fa-pinterest-p" aria-hidden="true"></i>
+            </a>
+        </li>
     </ul>
 
 
     <div class="details" style="height: 116px;">
-        <p>{{ $user->name }}</p>
-        <p><a href="{{ url('profile/inbox') }}">@lang('profile.inbox')</a></p>
-        <p>{{ $user->email }}</p>
+        <p>{{ $object->name }}</p>
+        <p><a href="{{ url('profile/Message/'. Auth::id()) }}">My messages</a></p>
+        <p>{{ $object->email }}</p>
+
+
     </div>
 </div>
 </div>
@@ -116,6 +129,7 @@
                 <div class="menu-name">@lang('profile.stores')</div>
             </a>
         </li>
+
         <li>
             <a href="{{ url('profile/upgrade') }}">
                 <div class="menu-name">@lang('profile.upgrade')</div>
@@ -126,5 +140,6 @@
                 <div class="menu-name">@lang('profile.settings')</div>
             </a>
         </li>
+
     </ul>
 </div>
